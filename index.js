584ccb631bdd90c843fe5268bc06e032fafdff68
Caryn Bailey <emptyvoices@hotmail.com>
2022-04-12 17:52:07 -0700

    commit tree 84977032d4a7830b32b035b8a3015608b6f84a18

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(a) {
+    return a+1; 
+}
+
+function decrement(a){
+    return a-1;
+}
+
+
+
+function makeInt(x) {
+    const parse = parseInt(x, 10);
+    return parse; 
+}
+
+function preserveDecimal(n) {
+   return parseFloat(n)
+    
+}
+
+
+   
+
+
+
+
+
